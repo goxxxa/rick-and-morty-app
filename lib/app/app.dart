3670fe262e5ad7e224933a00e749c08a845dd810
goxxxa <g.o.molodtsov@gmail.com>
2025-10-07 02:44:06 +0300

    refactor: Added App file and Page transition via Cubit

---
+++ lib/app/app.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/features/characters/bloc/characters_bloc.dart';
+import 'package:rick_and_morty_app/features/characters/bloc/characters_event.dart';
+import 'package:rick_and_morty_app/features/characters/view/characters_page.dart';
+import 'package:rick_and_morty_app/features/favorites/view/favorites_page.dart';
+import 'package:rick_and_morty_app/app/navigation_bloc.dart';
+
+class RickAndMortyApp extends StatelessWidget {
+  const RickAndMortyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => NavigationCubit(),
+      child: MaterialApp(
+        title: 'The Rick and Morty App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 39, 43, 51),
+          ),
+        ),
+        home: const MainNavigation(),
+      ),
+    );
+  }
+}
+
+class MainNavigation extends StatelessWidget {
+  const MainNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CharacterBLoC()..add(ChractersGetData()),
+      child: Scaffold(
+        body: IndexedStack(
+          index: context.watch<NavigationCubit>().state.index,
+          children: const [CharactersPage(), FavoritesPage()],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: context.watch<NavigationCubit>().state.index,
+          onTap: (index) =>
+              context.read<NavigationCubit>().updateTab(AppTab.values[index]),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

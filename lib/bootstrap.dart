1384fe396d9e16f9f24605a73710fbc479ee2317
goxxxa <g.o.molodtsov@gmail.com>
2025-10-13 00:10:42 +0300

    refactor: Updated bootstrap method, added new dependencies to pubspec.yaml.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,33 +1,50 @@
 import 'dart:async';
 
 import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:rick_and_morty_app/api/remote_api/api_client.dart';
 import 'package:rick_and_morty_app/app/app.dart';
-import 'package:rick_and_morty_app/app/app_observer.dart';
-import 'package:rick_and_morty_app/core/characters_cache.dart';
+import 'package:rick_and_morty_app/app/app_bloc_observer.dart';
 import 'package:rick_and_morty_app/core/database/database.dart';
-import 'package:rick_and_morty_app/core/database/db_provider.dart';
+import 'package:rick_and_morty_app/api/local_api/local_storage_character_api.dart';
+import 'package:rick_and_morty_app/repositories/characters/characters.dart';
 import 'package:talker/talker.dart';
 
 Future<void> bootstrap() async {
-  runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    final locator = GetIt.instance;
-    final talker = Talker();
+  final talker = Talker();
+  FlutterError.onError = (details) {
+    talker.error(details.exceptionAsString(), details.stack);
+  };
 
-    final database = AppDatabase();
-    final databaseProvider = DatabaseProviderImpl(db: database);
+  PlatformDispatcher.instance.onError = (error, stack) {
+    talker.error(error.toString(), stack);
+    return true;
+  };
 
-    Bloc.observer = AppObserver();
-    Bloc.transformer = sequential();
+  final locator = GetIt.instance;
+  locator.registerSingleton(talker);
 
-    locator.registerSingleton(database);
-    locator.registerSingleton(databaseProvider);
-    locator.registerSingleton(talker);
-    final cache = await CharactersCache.init();
-    locator.registerSingleton(cache);
-    runApp(const RickAndMortyApp());
-  }, (error, st) {});
+  Bloc.observer = AppObserver();
+  Bloc.transformer = sequential();
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
+
+  final db = AppDatabase();
+  final localApi = LocalStorageCharacterApi(db: db);
+  final remoteApi = RickAndMortyApiClient();
+
+  final repository = CharactersRepository(
+    localApi: localApi,
+    remoteApi: remoteApi,
+  );
+
+  runApp(App(createCharactersRepository: () => repository));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/widgets.dart';
 import 'package:rick_and_morty_app/bootstrap.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await bootstrap();
 }
